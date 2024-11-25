bcd4391e50fe375de8d3616e9bfb4af022b32340
Yuki Fujii <112536988+yukimeat1999@users.noreply.github.com>
2024-11-25 21:59:17 +0900

    Add files via upload

--- launch/CaveScan_Startup - コピー.bat
+++ launch/CaveScan_Startup - コピー.bat
@@ -1,9 +1,9 @@
-@echo off
+rem @echo off
 
 rem /////////////////////////// CaveScan_Startup.bat ///////////////////////////
 
 rem This file is the CaveScan startup batch file.
-rem Created by Y.Fujii on November 25, 2024.
+rem Created by Y.Fujii on November 1, 2023.
 rem License declaration 2.0.
 
 rem //////////////////////////////////////////////////////////////////////////
@@ -19,34 +19,30 @@ cd %LAUNCH_DIR%..
 set CaveScan_DIR=%CD%\
 
 rem If a computer name error occurs, set the following variables.
-rem set COMPUTERNAME=yuki-PC
+set COMPUTERNAME=DESKTOP
 
 rem Startup of RTC
-start "" /d %CaveScan_DIR%EtheURG\build\src\Debug EtheURGComp.exe
-start "" /d %CaveScan_DIR%MeasurementSystem\build\src\Debug MeasurementSystemComp.exe
+start "" /d %CaveScan_DIR%PointCloud_Reader\build\src\Debug PointCloud_ReaderComp.exe
 start "" /d %CaveScan_DIR%Registration\build\src\Debug RegistrationComp.exe
 start "" /d %CaveScan_DIR%Analyses\build\src\Debug AnalysesComp.exe
 start "" /d %CaveScan_DIR%PointCloud_Viewer\build\src\Debug PointCloud_ViewerComp.exe
-start "" /d %CaveScan_DIR%FPS\build\src\Debug FPSComp.exe
-start "" /d %CaveScan_DIR%WallDTC\build\src\Debug WallDTCComp.exe
-start "" /d %CaveScan_DIR%Contour\build\src\Debug ContourComp.exe
-start "" /d %CaveScan_DIR%MapViewer\build\src\Debug MapViewerComp.exe
-timeout 10
+timeout 20
 
 rem Automatic port connection
-cd %LAUNCH_DIR%
-rtcon ^
-/localhost/%COMPUTERNAME%.host_cxt/EtheURG0.rtc:range ^
-/localhost/%COMPUTERNAME%.host_cxt/MeasurementSystem0.rtc:range ^
---property dataport.interface_type=shared_memory
+rem cd %LAUNCH_DIR%
+cd %CaveScan_DIR%
+rem rtcon ^
+rem /localhost/%COMPUTERNAME%.host_cxt/EtheURG0.rtc:range ^
+rem /localhost/%COMPUTERNAME%.host_cxt/MeasurementSystem0.rtc:range ^
+rem --property dataport.interface_type=shared_memory
 
 rtcon ^
-/localhost/%COMPUTERNAME%.host_cxt/MeasurementSystem0.rtc:new_PointCloud ^
+/localhost/%COMPUTERNAME%.host_cxt/PointCloud_Reader0.rtc:File_PointCloud ^
 /localhost/%COMPUTERNAME%.host_cxt/Registration0.rtc:new_PointCloud ^
 --property dataport.interface_type=shared_memory
 
 rtcon ^
-/localhost/%COMPUTERNAME%.host_cxt/MeasurementSystem0.rtc:new_PointCloud ^
+/localhost/%COMPUTERNAME%.host_cxt/PointCloud_Reader0.rtc:File_PointCloud ^
 /localhost/%COMPUTERNAME%.host_cxt/PointCloud_Viewer0.rtc:new_PointCloud ^
 --property dataport.interface_type=shared_memory
 
@@ -69,33 +65,8 @@ rtcon ^
 /localhost/%COMPUTERNAME%.host_cxt/Analyses0.rtc:analyses_PointCloud ^
 /localhost/%COMPUTERNAME%.host_cxt/PointCloud_Viewer0.rtc:analyses_PointCloud ^
 --property dataport.interface_type=shared_memory
-
-rtcon ^
-/localhost/%COMPUTERNAME%.host_cxt/Registration0.rtc:merge_PointCloud ^
-/localhost/%COMPUTERNAME%.host_cxt/FPS0.rtc:PCD ^
---property dataport.interface_type=shared_memory
-
-rtcon ^
-/localhost/%COMPUTERNAME%.host_cxt/FPS0.rtc:DownPCD ^
-/localhost/%COMPUTERNAME%.host_cxt/WallDTC0.rtc:PCD ^
---property dataport.interface_type=shared_memory
-
-rtcon ^
-/localhost/%COMPUTERNAME%.host_cxt/FPS0.rtc:DownPCD ^
-/localhost/%COMPUTERNAME%.host_cxt/Contour0.rtc:PCD ^
---property dataport.interface_type=shared_memory
-
-rtcon ^
-/localhost/%COMPUTERNAME%.host_cxt/WallDTC0.rtc:PlanWall ^
-/localhost/%COMPUTERNAME%.host_cxt/MapViewer0.rtc:PlanWall ^
---property dataport.interface_type=shared_memory
-
-rtcon ^
-/localhost/%COMPUTERNAME%.host_cxt/Contour0.rtc:Contour ^
-/localhost/%COMPUTERNAME%.host_cxt/MapViewer0.rtc:Contour ^
---property dataport.interface_type=shared_memory
-
+timeout 20
 rem Automatically configuration
-rtconf /localhost/%COMPUTERNAME%.host_cxt/MeasurementSystem0.rtc set DEVICE_NAME \\.\COM5
+rem rtconf /localhost/%COMPUTERNAME%.host_cxt/MeasurementSystem0.rtc set DEVICE_NAME \\.\COM5
 
 endlocal
